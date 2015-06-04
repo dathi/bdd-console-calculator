9d525b1f41f319f7fd8b5fbeefa0353cda105f93
dathi <dathi@tma.com.vn>
2015-06-04 14:57:22 +0700

    resolve case dividend is zero. DONE!!!

--- calc.rb
+++ calc.rb
@@ -1,1 +1,11 @@
-print eval(ARGV[0])
+input = ARGV[0]
+if input.include? '/'
+	second_number = input.split('/')[1].to_i
+	if second_number != 0
+		print eval(input)
+	else
+		print 'error'
+	end
+else
+	print eval(input)
+end
